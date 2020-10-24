f639915e016cb228a937336eec319be45e9d2dd8
Astitva Nikose <astitvanikose.an@gmail.com>
2020-10-24 10:54:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@
 import json
 import requests
 
-subr = input('Enter the subreddit:')
-url = 'https://reddit.com/r/{}.json'.format(subr)
+url = 'https://reddit.com/r/wholesomememes.json'
 headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0'}
 r = requests.get(url, headers=headers)
 for post in r.json()['data']['children']:
